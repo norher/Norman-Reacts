6e659aaf5c49210df9e0af6b888765da69554ac1
Norman Herrera <norman.herrera@ymail.com>
2022-07-29 10:41:32 -0700

    worked on the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="d-flex justify-content-center align-items-center">
+      <a
+        className="align-items-center"
+        href="https://github.com/norher"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Github
+      </a>
+      <a
+        className="align-items-center"
+        href="https://www.linkedin.com/in/norman-herrera-dev/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        LinkedIn
+      </a>
+      <a
+        className="align-items-center"
+        href="https://www.instagram.com/norman.herrera/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Instagram
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
