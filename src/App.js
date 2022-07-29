d3e49c9ccac773de94da821ad5004fb6ea917f04
Norman Herrera <norman.herrera@ymail.com>
2022-07-28 22:01:55 -0700

    application reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import "./App.css";
+import React, { useState } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  // Render page based on state with About page as the default
+  const renderPage = () => {
+    if (currentPage === "Contact") {
+      return <Contact />;
+    } else if (currentPage === "Projects") {
+      return <Projects />;
+    } else if (currentPage === "About") {
+      return <About />;
+    }
+  };
+
+  const changePage = (page) => setCurrentPage(page);
+
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div className="bg-black">
+      <Navbar currentPage={currentPage} changePage={changePage} />
+      <main>{renderPage()}</main>
+      <Footer />
     </div>
   );
 }
